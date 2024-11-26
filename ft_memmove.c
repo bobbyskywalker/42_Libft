bcdf8852e1345e5f8c5e148b8579e3602abd1edd
Aleksander Garbacz <agarbacz@c3r2s4.42warsaw.pl>
2024-11-26 14:32:52 +0100

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,37 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 13:36:04 by agarbacz          #+#    #+#             */
-/*   Updated: 2024/11/26 13:56:03 by agarbacz         ###   ########.fr       */
+/*   Updated: 2024/11/26 14:32:19 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"  
-
-void    *memmove(void *dest, const void *src, size_t n)
+#include "libft.h"
+//TODO: to test PROPERLY :))
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    
+	unsigned int	i;
+	char			*d;
+	char			*s;
+
+	d = (char *)dest;
+	s = (const char *)src;
+	if (s > d)
+	{
+		i = 0;
+		while (s[i] && i < (int)n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = (int)n - 1;
+		while (d[i] && i >= 0)
+		{
+			d[i] = s[i];
+			i--;
+		}
+	}
+	return (dest);
 }
