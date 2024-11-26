307e5773a3ed61bbd3a58cd926e18a81fcf80b7b
Aleksander Garbacz <agarbacz@c3r2s4.42warsaw.pl>
2024-11-26 13:45:44 +0100

    init commit

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 13:30:26 by agarbacz          #+#    #+#             */
+/*   Updated: 2024/11/26 13:35:15 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (n-- > 0)
+		d[n] = s[n];
+	return (dest);
+}
---
+++ libft.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 13:02:15 by agarbacz          #+#    #+#             */
+/*   Updated: 2024/11/26 13:43:50 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+//SECTION: INCLUDES
+# include <string.h>
+
+//SECTION: PROTOTYPES
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_strlen(char *str);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+//TODO:
+void 	*ft_memmove(void *dest, const void *src, size_t n);
+
+#endif
