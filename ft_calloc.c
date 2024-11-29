89e619b3e8ac0bb8e05cc1ceedb9e4dfa53768c2
Aleksander Garbacz <agarbacz@c2r4s4.42warsaw.pl>
2024-11-29 15:18:21 +0100

    calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 14:52:11 by agarbacz          #+#    #+#             */
-/*   Updated: 2024/11/29 15:16:07 by agarbacz         ###   ########.fr       */
+/*   Updated: 2024/11/29 15:17:47 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		total = 1;
 	else
 		total = nmemb * size;
-	p = malloc(nmemb * size);
+	p = malloc(total);
 	if (!p)
 		return (NULL);
 	ft_bzero(p, total);
